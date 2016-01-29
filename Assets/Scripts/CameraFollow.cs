050f0648170bccf963ec29b8246616fe32170427
Sir-Baconn <smattia13579@gmail.com>
2016-01-29 01:05:54 -0500

    First Change
    Camera and movement added

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+
+    private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+
+        offset = transform.position - player.position;
+
+	}
+	
+	void LateUpdate () {
+
+        transform.position = player.position + offset;
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rbody;
+
+    private float speed = 13.0f;
+
+	void Start () {
+
+        rbody = GetComponent<Rigidbody>();
+
+	}
+	
+	void FixedUpdate () {
+
+        HandleMovement();
+
+	}
+
+    private void HandleMovement() {
+
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Vector3 movementVector = new Vector3(horizontalMovement, transform.position.y, verticalMovement);
+
+        rbody.AddForce(movementVector * speed);
+
+    }
+}
