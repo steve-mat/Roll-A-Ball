3a34bda7d98e5f2e816e69c2b0ef9b974ef0dc3c
Sir-Baconn <smattia13579@gmail.com>
2016-01-30 14:16:37 -0500

    Result
    Completed version of Roll-A-Ball project.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody rbody;
 
-    private float speed = 13.0f;
+    private float speed = 20.0f;
+
+    private int scoreCount;
+
+    private Text scoreText;
+
+    public Text winText;
 
 	void Start () {
 
         rbody = GetComponent<Rigidbody>();
+        scoreText = GameObject.FindGameObjectWithTag("Score Text").GetComponent<Text>();
+        scoreCount = 0;
+        SetScoreText();
 
 	}
 	
@@ -28,5 +37,21 @@ public class PlayerController : MonoBehaviour {
 
         rbody.AddForce(movementVector * speed);
 
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Pick Up")) {
+            Destroy(other.gameObject);
+            scoreCount++;
+            SetScoreText();
+        }
+    }
+
+    private void SetScoreText() {
+        scoreText.text = "Score: " + scoreCount.ToString();
+        if(scoreCount == 10) {
+            winText.gameObject.SetActive(true);
+            //Time.timeScale = 0f;  - freeze game after winning
+        }
     }
 }
